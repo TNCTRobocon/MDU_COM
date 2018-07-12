168db01983b8e192e758b3bbead38b122d4ea25a
terurin <hamusutaateru@live.jp>
2018-07-12 16:46:23 +0900

    refactor directorys

--- nbproject/Package-simulator.bash
+++ nbproject/Package-simulator.bash
@@ -10,9 +10,9 @@ CND_CONF=simulator
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/MDU_COMON.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=MDU_COMON.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=mducomon.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/MDU_COM.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=MDU_COM.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=mducom/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/mducomon.x/bin
+makeDirectory ${TMPDIR}/mducom/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/mducomon.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/mducom.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/mducomon.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/mducom.tar *
 checkReturnCode
 
 # Cleanup
